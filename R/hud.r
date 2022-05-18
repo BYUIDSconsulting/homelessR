00ce4cdc5d38bed38f1a2289313f88ecd1404698
J Hathaway <hathawayj@byui.edu>
2022-05-18 16:38:42 -0600

    base package

---
+++ R/hud.r
@@ -0,0 +1,13 @@
+#' @title download hud data
+#' @param url
+hr_download_hud <- function() {
+
+}
+
+#' @title download hud data
+#' @param url
+#' @examples 
+#' @export 
+hr_read_hud <- function(url) {
+    df <- hr_download_hud(url)
+}

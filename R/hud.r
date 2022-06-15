2ff5e963161bd4de1c911d1588b2d09a130f5fec
Juschultz33 <Juschultz33@gmail.com>
2022-06-15 16:45:28 -0600

    Update hud R to remove pipe

--- R/hud.r
+++ R/hud.r
@@ -20,23 +20,24 @@ hud_data <- function(y, url1= 'https://www.huduser.gov/portal/sites/default/file
 
 gather_hud_data <- function(){
   da2007 <- hud_data('2007')
-  columns = colnames(da2007)
-  da2008 <- hud_data('2008') %>% dplyr::select(columns)
-  da2009 <- hud_data('2009') %>% dplyr::select(columns)
-  da2010 <- hud_data('2010') %>% dplyr::select(columns)
-  da2011 <- hud_data('2011') %>% dplyr::select(columns)
-  da2012 <- hud_data('2012') %>% dplyr::select(columns)
-  da2013 <- hud_data('2013') %>% dplyr::select(columns)
-  da2014 <- hud_data('2014') %>% dplyr::select(columns)
-  da2015 <- hud_data('2015') %>% dplyr::select(columns)
-  da2016 <- hud_data('2016') %>% dplyr::select(columns)
-  da2017 <- hud_data('2017') %>% dplyr::select(columns)
-  da2018 <- hud_data('2018') %>% dplyr::select(columns)
-  da2019 <- hud_data('2019') %>% dplyr::select(columns)
-  da2020 <- hud_data('2020') %>% dplyr::select(columns)
+  columns <-  colnames(da2007)
+  da2008 <- dplyr::select(hud_data('2008'),columns)
+  da2009 <- dplyr::select(hud_data('2009'),columns)
+  da2010 <- dplyr::select(hud_data('2010'),columns)
+  da2011 <- dplyr::select(hud_data('2011'),columns)
+  da2012 <- dplyr::select(hud_data('2012'),columns)
+  da2013 <- dplyr::select(hud_data('2013'),columns)
+  da2014 <- dplyr::select(hud_data('2014'),columns)
+  da2015 <- dplyr::select(hud_data('2015'),columns)
+  da2016 <- dplyr::select(hud_data('2016'),columns)
+  da2017 <- dplyr::select(hud_data('2017'),columns)
+  da2018 <- dplyr::select(hud_data('2018'),columns)
+  da2019 <- dplyr::select(hud_data('2019'),columns)
+  da2020 <- dplyr::select(hud_data('2020'),columns)
   
   
   result <- rbind(da2007, da2008, da2009, da2010, da2011, da2012, da2013,
                   da2014, da2015, da2016, da2017, da2018, da2019, da2020) %>% 
     rename(state = State)
 }
+

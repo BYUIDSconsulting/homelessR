e0ad9fa4289bd4165e519a09908cddac1e1d4dc8
TheAdmrial <adamromriell@gmail.com>
2022-06-28 13:31:00 -0600

    updating the columns that are selected, renamed columns

--- R/bea2.r
+++ R/bea2.r
@@ -70,7 +70,8 @@ filter_year <- function(data, start = 0000, end = 9999) {
 #' @param data - the dataset from the bea.gov API that needs to be aggregated.
 #' @return returns the dataset aggregated to data values per state per year.  
 
-agg_state_year <- function(data) {
+agg_state_year <- function(data, type_of_dataset = '') {
+  col_name <- paste0("num_of_", type_of_dataset)
   return(data |>
            dplyr::mutate(DataValue = str_replace_all(DataValue, ',', ''), 
                          TimePeriod = as.numeric(TimePeriod), 
@@ -79,7 +80,9 @@ agg_state_year <- function(data) {
                          DataValue = tidyr::replace_na(DataValue, 0),
                          GeoName = stringr::str_replace(GeoName, '\\*', ''),
                          state = stringr::str_extract(GeoName, '[[:upper:]$]{2}')) |>
-           dplyr::select(-GeoFips, -GeoName, -NoteRef) |>
+           dplyr::rename(Year = TimePeriod) |>
+           dplyr::rename(col_name = DataValue) |>
+           dplyr::select(Year, col_name, state) |>
            dplyr::group_by(state, TimePeriod) |>
            dplyr::mutate(DataValue = sum(DataValue)) |>
            dplyr::distinct()
@@ -94,12 +97,11 @@ agg_state_year <- function(data) {
 #' @return The number of people that were employed for a year in a state to the desired years
 #' @export
 tot_employ_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
-  start <- Sys.time()
   dataset_name <- "Regional"
   dataset <- "CAEMP25N"
   line_code <- '10'
   
-  length_time <- Sys.time() - start
+  
   print(paste0('Making URL...', length_time))
   
   url <- make_get_url(dataset_name
@@ -107,12 +109,12 @@ tot_employ_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
                       , line_code
                       , api_key)
   
-  length_time <- Sys.time() - start
+  
   print('URL made!')
   print(paste0('Making API call...', length_time))
   
   req <- call_to_list(url)
-  length_time <- Sys.time() - start
+  
   print('Call made! Response received.')
   print(paste0('Formatting JSON to a dataframe...', length_time))
   
@@ -122,17 +124,17 @@ tot_employ_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
     r <- as.data.frame(req$BEAAPI$Results$Data[j])
     dat <- dplyr::bind_rows(dat,r)
   }
-  length_time <- Sys.time() - start
+  
   print('Formatting complete!')
   print(paste0('Aggregating to state and year...', length_time))
   
-  dat <- agg_state_year(data = dat)
+  dat <- agg_state_year(data = dat, 'jobs')
   
-  length_time <- Sys.time() - start
+  
   print('Filtering to desired year...')
   
   dat <- filter_year(data = dat, start = start_year, end = end_year)
-  length_time <- Sys.time() - start
+  
   print(paste0('Finished!', length_time))
   return(dat)
 }
@@ -145,12 +147,12 @@ tot_employ_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
 #' @export
 
 gdp_cur_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
-  start <- Sys.time()
+  
   dataset_name <- "Regional"
   dataset <- "CAGDP1"
   line_code <- '3'
   
-  length_time <- Sys.time() - start
+  
   print(paste0('Making URL...', length_time))
   
   url <- make_get_url(dataset_name
@@ -158,13 +160,13 @@ gdp_cur_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
                       , line_code
                       , api_key)
   
-  length_time <- Sys.time() - start
+  
   print('URL made!')
   print(paste0('Making API call...', length_time))
   
   req <- call_to_list(url)
   
-  length_time <- Sys.time() - start
+  
   print('Call made! Response received.')
   print(paste0('Formatting JSON to a dataframe...', length_time))
   
@@ -175,17 +177,16 @@ gdp_cur_bea <- function(api_key ='', start_year = 0000, end_year = 9999) {
     dat <- dplyr::bind_rows(dat,r)
   }
   
-  length_time <- Sys.time() - start
   print('Formatting complete!')
-  print(paste0('Aggregating to state and year...', length_time))
+  print('Aggregating to state and year...')
   
-  dat <- agg_state_year(data = dat)
-  length_time <- Sys.time() - start
+  dat <- agg_state_year(dat, 'gdp')
+  
   print('Filtering to desired year...')
   dat <- filter_year(data = dat, start = start_year, end = end_year)
   
-  length_time <- Sys.time() - start
-  print(paste0('Finished!', length_time))
+
+  print('Finished!')
   return(dat)
 }
 

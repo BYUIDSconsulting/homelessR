dee0aa404e9fd7394a022c7d03a981973929ad2f
TheAdmrial <adamromriell@gmail.com>
2022-07-14 16:13:02 -0600

    making the hud data callable in R

---
+++ R/hud_data.r
@@ -0,0 +1,12 @@
+#' Number of Homeless people per state per year. 
+#'
+#'
+#' @format Data frame with columns
+#' \describe{
+#' \item{state}{Full state name. }
+#' \item{Year}{Year of data observation}
+#' \item{Overall Homeless}{The total nubmer of homeless people in that state for that year.}
+#' }
+#' @examples
+#'   hud_data
+"hud_data"

e65e04c78efd77d3c30bde17693f2d41c31267ee
hunterwr <rog19011@byui.edu>
2022-07-16 16:06:29 -0600

    another attempt a changing case

--- R/crime_functions.R
+++ R/crime_functions.R
@@ -112,10 +112,11 @@ clean_data <- function(data, year){
   states <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", 
               "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming")
   df3 <- df2 |>
-    filter(df2$state %in% stringr::str_to_upper(states))
+    filter(df2$state %in% stringr::str_to_upper(states)) |>
+    mutate(state = (state.charAt(0) + tolowers(state.slice(1))))
   
   #change the case for each state
-  df3$state <- df3$state.charAt(0) + tolowers(df3$state.slice(1))
+  #df3$state <- df3$state.charAt(0) + tolowers(df3$state.slice(1))
   
   
   ## add a column with the year of data

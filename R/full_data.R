ba693de5c6bf09b85d2c91b10b57d73869b20373
hunterwr <rog19011@byui.edu>
2022-07-18 10:44:28 -0600

    roxygen rocks

---
+++ R/full_data.R
@@ -0,0 +1,12 @@
+#' A collection of all of our homelessness adjacent data sets. 
+#'
+#'
+#' @format Data frame with columns
+#' \describe{
+#' \item{state}{Full state name. }
+#' \item{Year}{Year of data observation}
+#' \item{Overall Homeless}{The total nubmer of homeless people in that state for that year.}
+#' }
+#' @examples
+#'   full_data
+"full_data"
